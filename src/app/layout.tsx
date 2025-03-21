ad4a9e8db555df8e4dc77706d6e3eaf3a60b0fdf
Eren <140344483+sysnern@users.noreply.github.com>
2025-03-21 08:51:41 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"; // SpeedInsights importu
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Vercel Speed Insights bileşeni */}
+        <SpeedInsights />
+
+        {/* Diğer içerikler */}
         {children}
       </body>
     </html>
